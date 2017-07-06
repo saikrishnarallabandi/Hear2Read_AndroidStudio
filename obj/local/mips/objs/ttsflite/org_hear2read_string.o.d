0c46e69b76b7a344206840bc71f8809ef9a4131c
Sai Krishna <srallaba@cs.cmu.edu>
2017-07-06 03:48:28 -0400

    Initial commit

---
+++ obj/local/mips/objs/ttsflite/org_hear2read_string.o.d
@@ -0,0 +1,4 @@
+./obj/local/mips/objs/ttsflite/org_hear2read_string.o: \
+  jni/org_hear2read_string.cc jni/./org_hear2read_string.h
+
+jni/./org_hear2read_string.h:
---
+++ obj/local/x86/objs/ttsflite/org_hear2read_string.o.d
@@ -0,0 +1,4 @@
+./obj/local/x86/objs/ttsflite/org_hear2read_string.o: \
+  jni/org_hear2read_string.cc jni/./org_hear2read_string.h
+
+jni/./org_hear2read_string.h:
